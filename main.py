bc6c568b2e40df942780f22c86c20cc0cc37cb0b
junya-f <fujiokajunya@icloud.com>
2024-12-24 06:21:00 +0900

    APIからのデータ取得

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+#ポケモンの名前とポケモンのidを取得
+@app.get("/pokemon/{id}")
+def get_pokemon(id: int):
+    response = requests.get("https://pokeapi.co/api/v2/pokemon/{id}")
+    if response.status_code == 200:
+        data = requests.json()
+        return {
+            "name": data["name"],
+            "image": data["sprites"]["front_default"]
+        }
+    return {"error" "Pokemon not found"}
